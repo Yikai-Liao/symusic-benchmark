190409c4d2cc1caeb083b639bcfb94000a78d0b8
lyk <lyk-boya@outlook.com>
2024-07-28 17:10:47 +0800

    add midifile binding

---
+++ binding/midifile.cpp
@@ -0,0 +1,26 @@
+#include "MidiFile.h"
+
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/string.h>
+
+namespace nb = nanobind;
+
+using namespace nb::literals;
+
+smf::MidiFile load(std::string file){
+    smf::MidiFile midifile;
+    midifile.read(file);
+    midifile.doTimeAnalysis();
+    midifile.linkNotePairs();
+    return midifile;
+}
+
+NB_MODULE(midifile_binding, m) {
+    m.doc() = "A simple binding of midifile for benchmark in python.";
+    nb::class_<smf::MidiFile>(m, "MidiFile")
+        .def("dump_midi", [](smf::MidiFile &midifile, const std::string& path){
+            midifile.write(path);
+        }, "Dump the midi file to the given path.")
+    ;
+    m.def("load", &load, "Load a midi file from the given path.");
+}
